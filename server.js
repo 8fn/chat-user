a3a535519bdbaaaf820fab09cebae056e74e9ff8
tsk <tanjilkh@gmail.com>
2022-12-30 21:03:33 +0000

    :zap: chat app

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const io = require('socket.io')(3000, { cors: { origin: true } })
 
+const users = {}
+
 io.on('connection', socket => {
-    socket.emit('chat-message', 'Hello World')
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message',{ message: message, name: users[socket.id] })
+    })
 })
